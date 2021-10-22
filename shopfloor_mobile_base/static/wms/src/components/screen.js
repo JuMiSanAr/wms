061da4db68316c55971444427eed911df84c7a12
Simone Orsi <simahawk@gmail.com>
2021-10-22 15:40:41 +0200

    sf_mobile_base: allow to inject menu items via registry

--- shopfloor_mobile_base/static/wms/src/components/screen.js
+++ shopfloor_mobile_base/static/wms/src/components/screen.js
@@ -174,7 +174,7 @@ Vue.component("Screen", {
 
 Vue.component("nav-items", {
     props: {
-        navigation: Array,
+        navigation: Object,
         show_full_info: {
             type: Boolean,
             default: true,
@@ -185,7 +185,7 @@ Vue.component("nav-items", {
     template: `
     <div :class="$options._componentTag">
         <v-list-item
-            v-for="item in navigation"
+            v-for="item in navigation.app_nav"
             :key="'nav-item-' + item.id"
             :to="{name: item.scenario, params: {menu_id: item.id, state: 'init'}}"
             link
@@ -199,10 +199,13 @@ Vue.component("nav-items", {
             </v-list-item-action>
         </v-list-item>
         <v-list-item
-            :key="'shop-products'"
-            :to="{name: 'products', params: {}}">
+            v-for="item in navigation.extra_nav"
+            :key="'nav-item-' + item.id"
+            :to="{name: item.to.name, params: item.to.params}"
+            link
+            >
             <v-list-item-content>
-                <nav-item-content :item="{'name': 'Products'}" :show_full_info="false" />
+                <nav-item-content :item="item" :show_full_info="false" />
             </v-list-item-content>
         </v-list-item>
     </v-btn>
